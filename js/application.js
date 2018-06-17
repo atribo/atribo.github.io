14f4e1385dd9e10b1dcb017867c6d44fa9d7ced6
avelar <fortunato.avelar@gmail.com>
2018-06-17 10:18:14 -0300

    created navegation include; created skills info page and index page

--- js/application.js
+++ js/application.js
@@ -1,16 +1,22 @@
+new Siema({
+  selector: '.character',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
 $( document ).ready(function() {
-  new Siema({
-    selector: '.character',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: false,
-    rtl: false,
-    onInit: () => {},
-    onChange: () => {},
+  $('.open-navegation').on('click', function() {
+    $('.navegation').fadeIn();
+  });
+  $('.close-navegation').on('click', function() {
+    $('.navegation').fadeOut();
   });
 });
