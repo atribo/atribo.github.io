ae49a20be8d7cdf37fb66201f8f9f29f9423ce7d
avelar <fortunato.avelar@gmail.com>
2018-06-17 14:43:08 -0300

    fixes

--- _site/js/application.js
+++ _site/js/application.js
@@ -1,17 +1,3 @@
-new Siema({
-  selector: '.character',
-  duration: 200,
-  easing: 'ease-out',
-  perPage: 1,
-  startIndex: 0,
-  draggable: true,
-  multipleDrag: true,
-  threshold: 20,
-  loop: false,
-  rtl: false,
-  onInit: () => {},
-  onChange: () => {},
-});
 $( document ).ready(function(){
   $('.open-navegation').on('click', function() {
     $('.navegation').fadeIn();
@@ -21,4 +7,31 @@ $( document ).ready(function(){
     $('.navegation').fadeOut();
     console.log("Clicado out");
   });
+
+  new Siema({
+    selector: '.character',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {
+      showSkillItem();
+    },
+  });
+
+  // function showSkillItem(){
+  //   $('.character-skill-list .skill-item').each(function(index){
+  //     var that = this;
+  //     var t = setTimeout(function() {
+  //         $(that).addClass("icon");
+  //     }, 100 * index);
+  //   });
+  // }
+
 });
